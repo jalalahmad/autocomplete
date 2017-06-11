b7d371ac5b8e1bed0eca861d35e5eb4e32983014
Jalal Ahmad <jalal.ahmad@gmail.com>
2017-06-10 20:59:36 -0400

    Added basic routes and added autocomplete dependency

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var stores = require('./routes/stores');
+var categories = require('./routes/categories');
+var products = require('./routes/products');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/stores', stores);
+app.use('/categories', categories);
+app.use('/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
